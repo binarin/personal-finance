ac02df26864bcafab67ac53f350a646dcee34e79
Alexey Lebedeff <binarin@binarin.ru>
2017-11-25 01:59:55 +0100

    Add live reload

--- lib/UI.hs
+++ lib/UI.hs
@@ -1,12 +1,40 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module UI where
 
-import qualified Graphics.UI.Threepenny       as UI
+import qualified Graphics.UI.Threepenny as UI
 import           Graphics.UI.Threepenny.Core
 
+import           System.Process (readProcess, callProcess)
+import           Control.Exception (try, SomeException)
+import           Control.Concurrent (threadDelay, forkIO)
+import           Control.Monad (forM_, void)
+import Text.Read (readMaybe)
+
 mkConfig :: IO Graphics.UI.Threepenny.Core.Config
-mkConfig = return defaultConfig { jsPort = Just 8023
+mkConfig = return defaultConfig { jsPort = Just 8024
                                 , jsStatic = Just "./static"
+                                , jsWindowReloadOnDisconnect = True
+                                , jsCustomHTML = Just "index.html"
                                 }
+kickFirefox :: IO ()
+kickFirefox = do
+    maybeWindows :: Either SomeException String <- try $ readProcess "xdotool" ["search", "--all", "--name", "Problem loading"] ""
+    case maybeWindows of
+      Left _ -> return ()
+      Right idStr -> forM_ (lines idStr) $ \windowId ->
+        case readMaybe windowId of
+          Nothing -> return ()
+          Just (n :: Integer) -> do
+              putStrLn $ show n
+              callProcess "xdotool" ["key", "--window", show n, "CTRL+R"]
+              return ()
+
+live :: IO ()
+live = do
+  void $ forkIO $ do
+      threadDelay 1000000
+      kickFirefox
+  main
 
 main :: IO ()
 main = do
@@ -15,5 +43,5 @@ main = do
 
 setup :: Window -> UI ()
 setup window = do
-    return window # set UI.title "Hello, World!"
+    return window # set UI.title "Hello, World!!!!"
     return ()
